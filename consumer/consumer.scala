b948f2ab5ed053cfb4cb62b227d2e8bea418a420
pau-anto <jappeloupantognelli@gmail.com>
2026-09-17 16:51:17 +0200

    added JPG decoding and writeStream to test

--- consumer/consumer.scala
+++ consumer/consumer.scala
@@ -27,6 +27,9 @@ object ImageClassifierConsumer {
     val maxFilesPerTrigger = 1
 
     // 1. Spark readStream surveille le dossier (+ checkpoint compare)
+    // 2. Détecte les nouveaux fichiers (automatique grâce au checkpoint)
+    // 3. Charger le contenu des nouveaux fichiers en binaire
+
     // Config readstream : 
         // - format : binaryFile (lire les octets bruts du JPG)
         // - options: maxFilesPerTrigger, recursiveFileLookup
@@ -52,12 +55,52 @@ object ImageClassifierConsumer {
         )
 
 
-    // 2. Détecte les nouveaux fichiers (automatique grâce au checkpoint)
-
-
-    // 3. Charger le contenu des nouveaux fichiers en binaire
-
     // 4. Décoder binaire JPG --> pixel numériques 
+    def decodeJPG(jpgBytes: Array[Byte]): Array[Array[Array[Int]]] = {
+    
+        try{
+            val inputStream = new ByteArrayInputStream(jpgBytes)
+
+            // ImageIO.rea() décode le JPG en BufferedImage
+            val bufferedImage = ImageIO.read(inputStream)
+
+            // récupérer les dimensions de l'image
+            val width = bufferedImage.getWidth 
+            val height = bufferedImage.getHeight
+
+            // Créer la matrice 3D pour stocker les pixels (hauteur x largeur x canaux)
+            val pixels = Array.ofDim[Int](height, width, 3) // 3 canaux pour RGB
+
+            // Boucler sur chaque pixel 
+            for (y <- 0 until height; x <- 0 until width) {
+                // getRGB() retourne un Int contenant ARGB
+                // Bits: [Alpha, Red, Green, Blue]
+                val rgb = bufferedImage.getRGB(x, y)
+
+                // Extraire les composantes RGB avec des opérations binaires
+                pixels(y)(x)(0) = (rgb >> 16) & 0xFF // Rouge
+                pixels(y)(x)(1) = (rgb >> 8) & 0xFF  // Vert
+                pixels(y)(x)(2) = rgb & 0xFF         // Bleu
+            }
+
+            pixels // retourner la matrice de pixels
+
+        } catch {
+            case e: Exception =>
+                println(s"Erreur lors du décodage du JPG: ${e.getMessage}")
+                Array.ofDim[Int](0, 0, 0) // retourner une matrice vide en cas d'erreur
+        }
+    }
+
+    // Enregistrer la fonction comme UDF pour l'utiliser dans les transformations Spark
+    val decodeJPGUDF = udf((bytes: Array[Byte]) => decodeJPG(bytes))
+
+    val decodedData = selectedData
+        .select(
+            col("image_path"), 
+            col("timestamp"), 
+            decodeJPGUDF(col("bytes_JPG")).as("pixels")
+        )
 
     // 5. Préparer les données pour le modèle ML
 
@@ -68,4 +111,20 @@ object ImageClassifierConsumer {
     // 8. Ecrire les prédictions dans un fichier de sortie
 
     // 9. Checkpoint sauvegarde l'état (automatique)
+
+    // Write stream (test)
+
+    val query = decodedData
+        .writeStream
+        .format("console") // pour le test, écrire dans la console
+        .mode("append") // mode append pour ajouter les nouvelles lignes
+        .option("checkpointLocation", checkpointDir) // définir le chemin du checkpoint
+        .option("numRows", 10) // nombre de lignes à afficher dans la console
+        .start()
+
+    println("✅ Consumer lancé - Décodage JPG actif")
+    println(s"📁 Source: $inputDir")
+    println(s"📁 Checkpoint: $checkpointDir")
+
+    query.awaitTermination()
 }
